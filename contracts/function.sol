0564b0b76468e22f9c3ab279a07fc3536c8ab7e0
Ishaq Nasir <ishaqnasir47@gmail.com>
2022-05-26 15:12:04 +0100

    func

---
+++ contracts/function.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract func {
+    uint public func;
+
+    function declare() public {
+        ....// solidity function is declared using this syntax and can be used to executee several commands
+    }
+
+}
